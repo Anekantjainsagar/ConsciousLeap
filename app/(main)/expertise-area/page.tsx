42e7467c4be505257e5d823f0a9e08e816cecd74
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2023-10-10 11:04:03 +0530

    Expertise area changed

--- app/(main)/expertise-area/page.tsx
+++ app/(main)/expertise-area/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import image from "@/Assets/AreaExpertise.jpg";
+import image from "@/(main)/Assets/AreaExpertise.jpg";
 
 const ExpertiseArea = () => {
   return (
