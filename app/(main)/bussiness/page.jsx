5dc4017792ac862eb879c6c96fc516db2ec3ee3a
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2023-11-14 19:02:21 +0530

    ASDF

--- app/(main)/bussiness/page.jsx
+++ app/(main)/bussiness/page.jsx
@@ -1,8 +1,19 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Line2 from "../Components/Lines/Line2";
+import ClientTestimonials from "../Components/ClientTestimonials";
 
 const BussinessConsciousleap = () => {
+  const [user, setUser] = useState({
+    name: "",
+    phone: "",
+    company: "",
+    noOfEmployees: "",
+    workEmail: "",
+    industry: "",
+    country: "",
+    about: "",
+  });
   useEffect(() => {
     const video = document.createElement("video");
     video.autoplay = true;
@@ -43,6 +54,87 @@ const BussinessConsciousleap = () => {
           Some of our esteemed clients use consciousleap for Business at their
           workplace.
         </p>
+      </div>
+      <Line2 />
+      <ClientTestimonials />
+      <Line2 />
+      <h1 className="text-2xl gradientHover cursor-pointer">Schedule a Call</h1>
+      <div className="mt-5 pb-10 flex flex-col items-center">
+        <input
+          type="text"
+          placeholder={"Name"}
+          value={user?.name}
+          onChange={(e) => {
+            setUser({ ...user, name: e.target.value });
+          }}
+          className="border outline-none px-4 py-1 w-[20vw] mb-2 rounded-md"
+        />
+        <input
+          type="text"
+          placeholder={"Phone no"}
+          value={user?.phone}
+          onChange={(e) => {
+            setUser({ ...user, phone: e.target.value });
+          }}
+          className="border outline-none px-4 py-1 w-[20vw] mb-2 rounded-md"
+        />
+        <input
+          type="text"
+          placeholder={"Company Name"}
+          value={user?.company}
+          onChange={(e) => {
+            setUser({ ...user, company: e.target.value });
+          }}
+          className="border outline-none px-4 py-1 w-[20vw] mb-2 rounded-md"
+        />
+        <input
+          type="text"
+          placeholder={"No Of Employees"}
+          value={user?.noOfEmployees}
+          onChange={(e) => {
+            setUser({ ...user, noOfEmployees: e.target.value });
+          }}
+          className="border outline-none px-4 py-1 w-[20vw] mb-2 rounded-md"
+        />
+        <input
+          type="text"
+          placeholder={"Work Email"}
+          value={user?.workEmail}
+          onChange={(e) => {
+            setUser({ ...user, workEmail: e.target.value });
+          }}
+          className="border outline-none px-4 py-1 w-[20vw] mb-2 rounded-md"
+        />
+        <input
+          type="text"
+          placeholder={"Industy"}
+          value={user?.industry}
+          onChange={(e) => {
+            setUser({ ...user, industry: e.target.value });
+          }}
+          className="border outline-none px-4 py-1 w-[20vw] mb-2 rounded-md"
+        />
+        <input
+          type="text"
+          placeholder={"Country"}
+          value={user?.country}
+          onChange={(e) => {
+            setUser({ ...user, country: e.target.value });
+          }}
+          className="border outline-none px-4 py-1 w-[20vw] mb-2 rounded-md"
+        />
+        <input
+          type="text"
+          placeholder={"How did you hear about us?"}
+          value={user?.about}
+          onChange={(e) => {
+            setUser({ ...user, about: e.target.value });
+          }}
+          className="border outline-none px-4 py-1 w-[20vw] mb-2 rounded-md"
+        />
+        <button className="w-[20vw] bg-websiteBlue text-white py-2 font-semibold rounded-lg">
+          Submit
+        </button>
       </div>
     </div>
   );
