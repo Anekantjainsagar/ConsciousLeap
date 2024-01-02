3b8e8dc10a9e1cc5bf9d6a165ed37c68cb459d13
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2024-01-02 10:50:15 +0530

    Added chatbot

--- app/layout.js
+++ app/layout.js
@@ -17,14 +17,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-	(function(I, L, T, i, c, k, s) {if(I.iticks) return;I.iticks = {host:c, settings:s, clientId:k, cdn:L, queue:[]};var h = T.head || T.documentElement;var e = T.createElement(i);var l = I.location;e.async = true;e.src = (L||c)+'/client/inject-v2.min.js';h.insertBefore(e, h.firstChild);I.iticks.call = function(a, b) {I.iticks.queue.push([a, b]);};})(window, 'https://cdn-v1.intelliticks.com/prod/common', document, 'script', 'https://app.intelliticks.com', 'Jpf3weRCe94WPCuMK_c', {});
-            `,
-          }}
-        ></script>
         <link
           rel="apple-touch-icon"
           sizes="180x180"
