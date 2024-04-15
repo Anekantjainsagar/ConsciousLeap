0f60d30c5bd62aa777320ddf83e9a39c24cb699a
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-04-15 17:15:54 +0530

    Added refund policies

--- app/(main)/privacy/refund/page.jsx
+++ app/(main)/privacy/refund/page.jsx
@@ -6,55 +6,261 @@ const Refund = () => {
       <h1 className="text-3xl font-bold text-websiteBlue mb-4">
         Rescheduling, Refund & Cancellation Policy
       </h1>
-      <ol className="list-decimal pl-10">
-        <li className="mb-6">
-          A minimum of 24 hours&apos; notice is required for rescheduling or
-          cancellation of an appointment. Without such notification, fees will
-          be charged to the clients as follows:
-          <ul className="list-disc pl-4">
+      <div className="px-2 md:px-4 mt-5">
+        <p className="font-bold underline text-lg mb-2">Therapy</p>
+        <ol className="list-decimal ml-5">
+          <li className="mb-1">
+            A minimum of 24 hours&apos; notice is required for rescheduling or
+            cancellation of an appointment. Without such notification, fees will
+            be charged to the clients as follows:
+            <ul className="list-disc pl-4">
+              <li>
+                Concellations within 24 hours of the appointment: 100% of the
+                fee payable for the session.
+              </li>
+              <li>
+                No-shows (missed appointment without notification): 100% of the
+                fee payable for the session.
+              </li>
+              <li>
+                The Client may avoid a cancellation or no-show fee if the
+                Parties are able to reschedule within the same week as dictated
+                by the therapist schedule and availability.
+              </li>
+            </ul>
+          </li>
+          <li className="mb-1">
+            In case of concellation by the therapist, or in the event, the
+            therapist with whom client has booked a paid session via the
+            consciousleap platform, has not been able to meet the client, the
+            client will need to write to us at therapy@consciousleap.co within
+            five (5) days from the occurrence of such an event; in which case,
+            100% fee paid for the session shall be refunded to the client within
+            the next six (6) business days in the original mode of payment done
+            by the client while booking, or alternatively, the therapist shall
+            adjust the fee paid towards a rescheduled session, as agreed to
+            between the parties.
+          </li>
+          <li className="mb-1">
+            To cancel or reschedule a session the client shall do so by logging
+            into the consciousleap platform and/or write an email at
+            (hello@consciousleap.co), any notification or communication beyond
+            office hours 10 om to 6 pm) shall be treated to be mode the
+            subsequent day.
+          </li>
+          <li className="mb-1">
+            clients will not be entitled to any refunds in cases where, the
+            therapist is unable to meet the client at the exact time of the
+            scheduled oppointment time and the client is required to woit,
+            irrespective of the fact whether the client is required to wait or
+            choose to not obtain the medical consciousleap platform from the
+            said therapist
+          </li>
+        </ol>
+        <p className="font-bold underline text-lg mb-2 mt-4">Conscious Store</p>
+        <div className="px-2">
+          Our returns and exchange policy is subject to goods sold by
+          consciousleap’'s verified brands and designers directly to you, with
+          no exceptions for third-party sales. All policy changes will reflect
+          on this page.
+          <br />
+          In brief, we will offer a full refund if the item(s) were paid for
+          using a debit/credit card and offer store credit should the payment
+          mode chosen be COD. For this purpose, we must receive the sold
+          merchandise within 14 days from the delivery date.
+          <br />
+          Goods may only be accepted if they are not damaged, used, or washed
+          and in original packaging unless damaged.
+          <br />
+          The Policy envisages the terms and conditions under which:
+          <ul className="ml-4 list-disc">
             <li>
-              Concellations within 24 hours of the appointment: 100% of the fee
-              payable for the session.
+              You are permitted to return goods purchased by you, subject to a
+              valid reason;
             </li>
             <li>
-              No-shows (missed appointment without notification): 100% of the
-              fee payable for the session.
+              A refund may be obtained for goods purchased by you, subject to
+              certain conditions;
             </li>
             <li>
-              The Client may avoid a cancellation or no-show fee if the Parties
-              are able to reschedule within the same week as dictated by the
-              therapist schedule and availability.
+              You may cancel orders for goods purchased by you within a
+              stipulated period of time.
             </li>
           </ul>
-        </li>
-        <li className="mb-6">
-          In case of concellation by the therapist, or in the event, the
-          therapist with whom client has booked a paid session via the
-          consciousleap platform, has not been able to meet the client, the
-          client will need to write to us at therapy@consciousleap.co within
-          five (5) days from the occurrence of such an event; in which case,
-          100% fee paid for the session shall be refunded to the client within
-          the next six (6) business days in the original mode of payment done by
-          the client while booking, or alternatively, the therapist shall adjust
-          the fee paid towards a rescheduled session, as agreed to between the
-          parties.
-        </li>
-        <li className="mb-6">
-          To cancel or reschedule a session the client shall do so by logging
-          into the consciousleap platform and/or write an email at
-          (hello@consciousleap.co), any notification or communication beyond
-          office hours 10 om to 6 pm) shall be treated to be mode the subsequent
-          day.
-        </li>
-        <li className="mb-6">
-          clients will not be entitled to any refunds in cases where, the
-          therapist is unable to meet the client at the exact time of the
-          scheduled oppointment time and the client is required to woit,
-          irrespective of the fact whether the client is required to wait or
-          choose to not obtain the medical consciousleap platform from the said
-          therapist
-        </li>
-      </ol>
+          It is important to note while the Company makes its best efforts to
+          ensure that returns, refunds, and cancellations are available to the
+          users of the Website, the Company has to place certain restrictions on
+          the same in order to meet its business, legal, and contractual
+          obligations.
+          <br />
+          Return, refunds, or cancellations may also vary from brand to brand or
+          seller to seller, in which case each such good sold thereunder will
+          have specific guidelines which will determine the terms and conditions
+          related to returns, refunds, or cancellations (the “Guidelines”).
+          <br />
+          In each such case, the Guidelines shall prevail over this Policy and
+          it is important for you to acquaint yourself with the applicable
+          Guidelines while making a purchase on the Website.
+          <br />
+          Lastly, you agree, abide and acknowledge that consciousleap’s decision
+          with regard to the Refund & Returns Policy shall be final and binding
+          to you and that you shall not be entitled to raise any claims,
+          damages, losses, liabilities, etc. whatsoever nature in that regards
+          once consciousleap has notified you about its decision.
+          <p className="font-bold underline mt-5 mb-2">
+            What Can I Return And When?
+          </p>
+          <ul className="ml-4 list-disc">
+            <li>You receive a faulty or defective product</li>
+            <li>
+              You receive a product that is different from what you ordered
+            </li>
+            <li>If the product does not fit well (size issues)</li>
+          </ul>
+          <p className="font-bold underline mt-5 mb-2">
+            What Products Can NOT Be Returned?
+          </p>
+          <ul className="list-disc ml-4">
+            <li>Items that have been worn, washed, or soiled</li>
+            <li>
+              Electronic items, once the seal has been broken, cannot be
+              returned unless the equipment is faulty
+            </li>
+            <li>
+              Consumable products like food and beverages, bath and beauty, or
+              personal hygiene products
+            </li>
+            <li>
+              Bulky items like furniture, statues, paintings, temples (mandir)
+            </li>
+            <li>Custom-made or hand-made products</li>
+            <li>Items marked as not-returnable</li>
+            <li>
+              No refund, exchange, or return on discount/sale items or when
+              discount codes are used in the order
+            </li>
+            <li>
+              No refund on items purchased by the cash-on-delivery payment
+              option
+            </li>
+          </ul>
+          <p className="font-bold underline mt-5 mb-2">
+            Conditions For Returning Products:
+          </p>
+          <ul className="list-disc ml-4">
+            <li>
+              The products must be returned in the original condition they were
+              received in, along with any bills, tags, labels, etc.
+            </li>
+            <li>Products must be in their original form and unsoiled</li>
+            <li>Brand packaging should be intact</li>
+            <li>
+              Replacement of a product will be subject to the availability of
+              the product on the website at that time
+            </li>
+            <li>
+              The Product should have been bought with a prepaid payment option
+              only
+            </li>
+          </ul>
+          <p className="font-bold underline mt-5 mb-2">
+            Conscious Store Return, Exchange & Refund Guidelines:
+          </p>
+          <ul className="list-disc ml-4">
+            <li>
+              There will be no returns or exchanges for products made as per
+              custom requirements.
+            </li>
+            <li>
+              There will be no refunds, returns, or exchanges for products on
+              discount/sale or when discount codes are used in the order.
+            </li>
+            <li>
+              There will be no refunds for products paid via the Cash on
+              Delivery method. Store credit will be provided in such cases.
+            </li>
+            <li>
+              You can create a return/exchange request by email quoting the
+              order number. Email to{" "}
+              <a href="mailto:support@consciousleap.co">
+                support@consciousleap.co
+              </a>
+              .
+            </li>
+            <li>
+              Sensitive cases will receive a call back from the team at
+              consciousleap.
+            </li>
+            <li>
+              Your email must contain the name of the product and the order ID
+              along with a detailed description of the nature of the concern,
+              and pictures/videos of the product in case of damages or
+              manufacturing defects.
+            </li>
+            <li>
+              The email must be sent within 3 days of receipt of the product.
+              Disputes registered after 3 days of delivery shall be entertained
+              at the sole discretion of the seller of the product.
+            </li>
+            <li>
+              If the seller agrees to a refund, then you must send the product
+              back to the vendor within 7 days of the vendor agreeing to the
+              refund.
+            </li>
+            <li>
+              Please ship your return/exchange order to the shipping address
+              communicated to you via email. This tends to vary based on the
+              product. This cost will have to be borne by you.
+            </li>
+            <li>
+              Please note, that should the fault be ours (in case of a
+              manufacturing defect), we will contact you to arrange a pickup. In
+              all other cases, please note that we may only process exchanges
+              and returns once you have shipped us the product.
+            </li>
+            <li>
+              If the goods are delivered to you in a damaged condition, please
+              email us at{" "}
+              <a href="mailto:support@consciousleap.co">
+                support@consciousleap.co
+              </a>{" "}
+              with a photograph of the damaged product, the invoice details, and
+              the barcode on the package.
+            </li>
+            <li>
+              Exchanges are subject to the discretion of a mindful team at
+              consciousleap.
+            </li>
+            <li>
+              Size change requests are considered for exchange first and only
+              refunded (store credit if COD/direct refund if debit/credit card)
+              should no alternative size be available.
+            </li>
+            <li>
+              As consciousleap stands for exclusivity, many of our products are
+              offered in limited sizes. Please note this may vary by
+              designer/brand. Please check the product details for any
+              anomalies.
+            </li>
+            <li>
+              Refunds are processed within one week of receiving the returned
+              goods.
+            </li>
+            <li>
+              We can only process refunds for exchanges or returns once we have
+              received the product. Typically this timeline is 5-7 days from the
+              date of return shipping.
+            </li>
+            <li>
+              Returned goods should retain their original tags. Goods that are
+              damaged/without tags will be returned to the customer. We hope it
+              doesn't come to this, but should the goods returned to us by you
+              fail to meet our returns standards, the cost of returning them to
+              you will have to be borne by you.
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
