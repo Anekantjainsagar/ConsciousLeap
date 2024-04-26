dbb728dbf39a28aaf8401dca3533fb468dd6a164
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-04-26 17:11:52 +0530

    Added teh company name in the footer section of teh website

--- app/(main)/Components/Utils/Footer.js
+++ app/(main)/Components/Utils/Footer.js
@@ -434,10 +434,11 @@ const Footer = () => {
             src={certified}
             alt="Certified"
             className="w-[30vw] md:w-[8vw]"
-          />
+          /> 
         </div>
         <p className="mt-3 mb-16 text-center md:text-start text-sm font-light">
-          Copyright © 2023 consciousleap, all rights reserved.
+          Coleco Wellbeing OPC PVT LTD
+          <br /> Copyright © 2023 consciousleap, all rights reserved.
         </p>
       </div>
     </>
