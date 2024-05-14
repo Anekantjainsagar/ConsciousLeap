4c66234822252594909a4134acae0a6989590a51
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-05-14 11:47:24 +0530

    Updated the ml url

--- app/Utils/urls.js
+++ app/Utils/urls.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:5000/api";
 export const BASE_URL = "https://consciousleap.co:5000/api";
-export const ML_URL = "http://consciousleap.co:8000/find_similarity";
-// export const ML_URL = "http://localhost:5000/find_similarity";
+// export const ML_URL = "http://consciousleap.co:8000/find_similarity";
+export const ML_URL = "https://consciousleapml.onrender.com//find_similarity";
