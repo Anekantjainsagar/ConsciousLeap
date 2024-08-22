949de840ba89ca7d23bbdf31127ed17d6da6d724
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-08-22 20:11:55 +0530

    Added react-ga

--- app/(main)/Components/GoogleTagManager.jsx
+++ app/(main)/Components/GoogleTagManager.jsx
@@ -1,29 +1,13 @@
 "use client";
 import { useEffect } from "react";
+import ReactGA from "react-ga";
 
 const GoogleAnalytics = () => {
+  f;
+  ReactGA.initialize("G-5SMJLE8MGE");
+
   useEffect(() => {
-    // Create the script element for Google Analytics
-    const script = document.createElement("script");
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-5SMJLE8MGE";
-    script.async = true;
-    document.head.appendChild(script);
-
-    // Create the inline script to configure Google Analytics
-    const inlineScript = document.createElement("script");
-    inlineScript.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-5SMJLE8MGE');
-    `;
-    document.head.appendChild(inlineScript);
-
-    // Clean up the scripts when the component unmounts
-    return () => {
-      document.head.removeChild(script);
-      document.head.removeChild(inlineScript);
-    };
+    ReactGA.pageview(window.location.pathname);
   }, []);
 
   return null; // This component does not render anything
