9f81c987246a5881e44a637a838b054d09bb68fc
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-08-23 17:45:33 +0530

    Added some logs

--- app/(main)/Components/GoogleTagManager.jsx
+++ app/(main)/Components/GoogleTagManager.jsx
@@ -6,8 +6,17 @@ const GoogleAnalytics = () => {
   let TRACKING_ID = "G-S9TJGS7CP3";
 
   useEffect(() => {
-    ReactGA.initialize(TRACKING_ID);
+    ReactGA.initialize(TRACKING_ID, {
+      debug: true, // Enable debug mode
+    });
+    console.log("Google Analytics Initialized");
+
+    // Log initial page view
     ReactGA.pageview(window.location.pathname + window.location.search);
+    console.log(
+      "Page View Logged:",
+      window.location.pathname + window.location.search
+    );
   }, []);
 
   return null;
