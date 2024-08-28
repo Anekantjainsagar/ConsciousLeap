765ee6b2a485860263109396bf2fcc8c89b9abaa
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-08-28 15:22:25 +0530

    Updated new code

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,14 +1,38 @@
 import Navbar from "./Components/Utils/Navbar";
 import Footer from "./Components/Utils/Footer";
 import GoogleAnalytics from "./Components/GoogleTagManager";
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
   return (
-    <section lang="en">
-      <Navbar />
-      <GoogleAnalytics />
-      {children}
-      <Footer />
-    </section>
+    <>
+      <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M277TQXF');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </Head>
+
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M277TQXF"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        <Navbar />
+        <GoogleAnalytics />
+        {children}
+        <Footer />
+      </body>
+    </>
   );
 }
