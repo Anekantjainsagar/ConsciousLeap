22df5467e4112e7b0ca323d144b504c6931b626b
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-08-29 16:59:26 +0530

    Updated

--- app/(main)/Components/Authorized/Captcha.js
+++ app/(main)/Components/Authorized/Captcha.js
@@ -5,7 +5,7 @@ const Captcha = ({ captchaRef }) => {
   return (
     <ReCAPTCHA
       className="recaptcha my-2"
-      sitekey={" 6Ldyj4knAAAAAJPvb8sjKBv2VxS19xMTHkHc63Ho"}
+      sitekey={"6Ldyj4knAAAAAJPvb8sjKBv2VxS19xMTHkHc63Ho"}
       ref={captchaRef}
     />
   );
