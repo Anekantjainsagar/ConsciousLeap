655c98fff69ed6d871191db0f47b742bbf1f95e5
Anekantjainsagar <anekantjainsagar@gmail.com>
2025-03-24 20:04:59 +0530

    Updated

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,27 +1,30 @@
+import Script from "next/script";
 import Navbar from "./Components/Utils/Navbar";
 import Footer from "./Components/Utils/Footer";
-import Head from "next/head";
 
 export default function RootLayout({ children }) {
   return (
     <>
-      <Head>
-        {/* Google Tag Manager */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-R4M3WS91NM"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-R4M3WS91NM');
-            `,
-          }}
-        ></script>
-      </Head>
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-PS1GHQGHRV`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PS1GHQGHRV', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <Navbar />
       {children}
       <Footer />
